4a1786a0aed1f03b36756954f4d8f5c6245f8578
sota <sotacodeprivate@gmail.com>
2025-02-23 18:18:09 +1300

    Update default.tsx
    footer updated

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,7 +1,7 @@
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import { Head } from "./head";
-import { GithubIcon, InstagramIcon, LinkedInIcon } from "@/components/icons";
+import { GithubIcon, InstagramIcon, LinkedInIcon, YouTubeIcon } from "@/components/icons";
 import { siteConfig } from "@/config/site";
 import { button as buttonStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
@@ -16,32 +16,11 @@ export default function DefaultLayout({
 		<div className={`${tektur.className} antialiased flex flex-col`}>
 			<Head />
 			<Navbar />
-			<main className="container max-w-7xl px-6 flex-grow">
+			<main className="container max-w-7xl px-6 flex-grow my-8">
 				{children}
 			</main>
 			<footer className="w-full flex items-center justify-center py-4">
-				{/* <Link
-					isExternal
-					className="flex items-center gap-1 text-current"
-					href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-					title="nextui.org homepage"
-				>
-					<span className="text-default-600">Powered by</span>
-					<p className="text-primary">NextUI</p>
-				</Link> */}
 				<div className="flex gap-3 mt-20">
-					{/* <Link
-						isExternal
-						as={NextLink}
-						href={siteConfig.links.docs}
-						className={buttonStyles({
-							color: "primary",
-							radius: "full",
-							variant: "shadow",
-						})}
-					>
-						Documentation
-					</Link> */}
 					<Link
 						isExternal
 						as={NextLink}
@@ -51,15 +30,24 @@ export default function DefaultLayout({
 						<LinkedInIcon size={20} />
 						LinkedIn
 					</Link>
-					{/* <Link
+					<Link
+						isExternal
+						as={NextLink}
+						className={buttonStyles({ variant: "bordered", radius: "full" })}
+						href={siteConfig.links.youtube}
+					>
+						<YouTubeIcon style={{ color: '#FF0000' }} size={20} />
+						Youtube
+					</Link>
+					 <Link
 						isExternal
 						as={NextLink}
 						className={buttonStyles({ variant: "bordered", radius: "full" })}
 						href={siteConfig.links.instagram}
 					>
-						<InstagramIcon size={20} />
+						<InstagramIcon size={20} style={{ color: '#E4405F' }} />
 						Instagram
-					</Link> */}
+					</Link> 
 					<Link
 						isExternal
 						as={NextLink}
@@ -70,15 +58,6 @@ export default function DefaultLayout({
 						GitHub
 					</Link>
 				</div>
-				{/* <Link isExternal href={siteConfig.links.linkedin}>
-						<LinkedInIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.instagram}>
-						<InstagramIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.github}>
-						<GithubIcon className="text-default-500" />
-					</Link> */}
 			</footer>
 		</div>
 	);
