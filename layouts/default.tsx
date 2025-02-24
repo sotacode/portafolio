1acc7efeb78cd7f45bdd4106cd4f537f00c9f7ff
sota <sotacodeprivate@gmail.com>
2025-02-24 15:45:35 +1300

    footer fixed

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -20,43 +20,51 @@ export default function DefaultLayout({
 				{children}
 			</main>
 			<footer className="w-full flex items-center justify-center py-4">
-				<div className="flex gap-3 mt-20">
-					<Link
-						isExternal
-						as={NextLink}
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.linkedin}
-					>
-						<LinkedInIcon size={20} />
-						LinkedIn
-					</Link>
-					<Link
-						isExternal
-						as={NextLink}
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.youtube}
-					>
-						<YouTubeIcon style={{ color: '#FF0000' }} size={20} />
-						Youtube
-					</Link>
-					 <Link
-						isExternal
-						as={NextLink}
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.instagram}
-					>
-						<InstagramIcon size={20} style={{ color: '#E4405F' }} />
-						Instagram
-					</Link> 
-					<Link
-						isExternal
-						as={NextLink}
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.github}
-					>
-						<GithubIcon size={20} />
-						GitHub
-					</Link>
+				<div className="sm:flex sm:flex-col lg:flex mt-20 justify-center items-center">
+					<div className="">
+						<Link
+							isExternal
+							as={NextLink}
+							className={buttonStyles({ variant: "bordered", radius: "full",})}
+							style={{ marginLeft: '5px', marginRight: '5px', width: '130px', marginBottom: '10px' }}
+							href={siteConfig.links.linkedin}
+						>
+							<LinkedInIcon size={20} />
+							LinkedIn
+						</Link>
+						<Link
+							isExternal
+							as={NextLink}
+							className={buttonStyles({ variant: "bordered", radius: "full" })}
+							style={{ marginLeft: '5px', marginRight: '5px', width: '130px', marginBottom: '10px' }}
+							href={siteConfig.links.youtube}
+						>
+							<YouTubeIcon style={{ color: '#FF0000' }} size={20} />
+							Youtube
+						</Link>
+					</div>
+					<div>
+						<Link
+							isExternal
+							as={NextLink}
+							className={buttonStyles({ variant: "bordered", radius: "full" })}
+							style={{ marginLeft: '5px', marginRight: '5px', width: '130px', marginBottom: '10px' }}
+							href={siteConfig.links.instagram}
+						>
+							<InstagramIcon size={20} style={{ color: '#E4405F' }} />
+							Instagram
+						</Link>
+						<Link
+							isExternal
+							as={NextLink}
+							className={buttonStyles({ variant: "bordered", radius: "full" })}
+							style={{ marginLeft: '5px', marginRight: '5px', width: '130px', marginBottom: '10px' }}
+							href={siteConfig.links.github}
+						>
+							<GithubIcon size={20} />
+							GitHub
+						</Link>
+					</div>
 				</div>
 			</footer>
 		</div>
