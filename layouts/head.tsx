01b4ede2ab8d20a8cd1e5e75380fe3af252b5581
sota <sotacodeprivate@gmail.com>
2025-03-11 23:25:23 +1300

    updated again

--- layouts/head.tsx
+++ layouts/head.tsx
@@ -10,7 +10,7 @@ export const Head = () => {
 			<title>{siteConfig.name}</title>
 			<meta key="title" content={siteConfig.name} property="og:title" />
 			{
-				language  == "ES" ?
+				language === "ES" ? // Fixed equality comparison operator from == to ===
 				<>
 					<meta content={siteConfig.texts.description.ES} property="og:description" />
 					<meta content={siteConfig.texts.description.ES} name="description" />
@@ -20,14 +20,18 @@ export const Head = () => {
 					<meta content={siteConfig.texts.description.EN} property="og:description" />
 					<meta content={siteConfig.texts.description.EN} name="description" />
 				</>
-
 			}
 			<meta
 				key="viewport"
 				content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
 				name="viewport"
 			/>
-			<link href="/sotacodeicon.webp" rel="icon" />
+			{/* <link href="/sotacodeicon.webp" rel="icon" /> */}
+			
+			{/* Open Graph / Social Media Image Tags */}
+			<meta property="og:image" content="/home/sotacodeicon.webp" />
+			<meta property="og:image:width" content="650" />
+			<meta property="og:image:height" content="650" />
 		</NextHead>
 	);
 };
